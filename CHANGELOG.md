a6d19cbf3348f0210a48d58b70d33d43ce0281fd
adamzerella <adamzerella@users.noreply.github.com>
2018-11-10 17:22:45 +1100

    Added DOM nightmode manipulation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.0
+-   Added event listener for if the current browser tab is active.
+-   Added script to change DOM to "night-mode" ( WIP )
+
 # 0.1.0
 -   Added boilerplate function doc
 -   Setup core logic on when to switch to dark mode, currently based off of >= 6PM local time.
--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ navigator.geolocation.watchPosition( configureTheme, handleError );
 function configureTheme( position ) {
     // Current time in Date() format.
     let currentTime = new Date( position.timestamp );
-
+  
     // If local time is between 6PM - 6AM, 18->0 || 0-6.
     if ( currentTime.getHours() >= 18 || currentTime.getHours() <= 6) {
         console.log(`Switching to darkmode...`);
+
+        chrome.tabs.onActivated.addListener( tab => {
+            let jsOptions = {
+                file: "src/style.js",
+                allFrames: true,
+                matchAboutBlank: true,
+                runAt: "document_idle"
+            }
+        
+            chrome.tabs.executeScript(jsOptions)
+        });
     }
     else {
         console.log(`Switching to lightmode...`);
@@ -24,5 +35,4 @@ function configureTheme( position ) {
  */
 function handleError( error ) {
     console.error( error );
-    throw new Error( error );
 }
